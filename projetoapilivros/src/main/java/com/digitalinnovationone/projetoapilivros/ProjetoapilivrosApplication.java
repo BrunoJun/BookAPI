6fd3e7d4d8e08cddb00e7560a59f336535dc52df
BrunoJun-commits <brunojun400@gmail.com>
2021-07-04 12:07:05 -0300

    Inicio do projeto

---
+++ projetoapilivros/src/main/java/com/digitalinnovationone/projetoapilivros/ProjetoapilivrosApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.projetoapilivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoapilivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoapilivrosApplication.class, args);
+	}
+
+}
---
+++ projetoapilivros/src/test/java/com/digitalinnovationone/projetoapilivros/ProjetoapilivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.projetoapilivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoapilivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
